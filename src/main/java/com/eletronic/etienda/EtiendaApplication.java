0fa51b1ebcb1781b8b3bb6479cae6bf45c5c8c21
CesarsEren <cpinedo428@gmail.com>
2021-01-13 13:36:43 -0500

    first commit

---
+++ src/main/java/com/eletronic/etienda/EtiendaApplication.java
@@ -0,0 +1,13 @@
+package com.eletronic.etienda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EtiendaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EtiendaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eletronic/etienda/EtiendaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eletronic.etienda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EtiendaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
